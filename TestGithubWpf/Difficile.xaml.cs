1981de4af4c5357c8908b010486c06f2e45ae4a9
Alina Dvornyk <alina.dvornyk@etu.univ-smb.fr>
2024-01-15 16:58:35 +0100

    mod

--- TestGithubWpf/Difficile.xaml.cs
+++ TestGithubWpf/Difficile.xaml.cs
@@ -11,6 +11,7 @@ using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Shapes;
+using System.Windows.Threading;
 
 namespace TestGithubWpf
 {
@@ -19,9 +20,225 @@ namespace TestGithubWpf
     /// </summary>
     public partial class Difficile : Window
     {
+        DispatcherTimer gameTimer = new DispatcherTimer();
+        bool goLeft, goRight, goDown, goUp;
+        int speed = 7;
+        Rect pacmanHitBox;
+        int ghostSpeed = 10;
+        int ghostMoveStep = 160;
+        int currentGhostStep;
+        int score = 0;
+        bool gameover = false;
+        bool isGamePaused = false;
+        ImageBrush starImage = new ImageBrush();
+        List<Rectangle> itemRemover = new List<Rectangle>();
+
         public Difficile()
         {
             InitializeComponent();
+            GameSetUp();
+        }
+        private void GameSetUp()
+        {
+
+            MyCanvas.Focus();
+            gameTimer.Tick += GameLoop;
+            gameTimer.Interval = TimeSpan.FromMilliseconds(20);
+            gameTimer.Start();
+            currentGhostStep = ghostMoveStep;
+
+            //pacmanHitBox = new Rect(Canvas.GetLeft(pacman), Canvas.GetTop(pacman), pacman.Width, pacman.Height);
+        }
+        private void movePacman()
+        {
+            if (goRight && Canvas.GetLeft(pacman) < Application.Current.MainWindow.Width - 60)
+            {
+                Canvas.SetLeft(pacman, Canvas.GetLeft(pacman) + speed);
+            }
+            if (goLeft && Canvas.GetLeft(pacman) > 20)
+            {
+                Canvas.SetLeft(pacman, Canvas.GetLeft(pacman) - speed);
+            }
+            if (goUp && Canvas.GetTop(pacman) > 20)
+            {
+                Canvas.SetTop(pacman, Canvas.GetTop(pacman) - speed);
+            }
+            if (goDown && Canvas.GetTop(pacman) < Application.Current.MainWindow.Height - 60)
+            {
+                Canvas.SetTop(pacman, Canvas.GetTop(pacman) + speed);
+            }
+
+            pacmanHitBox = new Rect(Canvas.GetLeft(pacman), Canvas.GetTop(pacman), pacman.Width, pacman.Height);
+            foreach (var x in MyCanvas.Children.OfType<Rectangle>())
+            {
+                Rect hitBox = new Rect(Canvas.GetLeft(x), Canvas.GetTop(x), x.Width, x.Height);
+
+                if ((string)x.Tag == "star")
+                {
+                    if (pacmanHitBox.IntersectsWith(hitBox) && x.Visibility == Visibility.Visible)
+                    {
+                        x.Visibility = Visibility.Hidden;
+                        score++;
+                    }
+                }
+
+                if ((string)x.Tag == "wall")
+                {
+                    if (pacmanHitBox.IntersectsWith(hitBox))
+                    {
+                        if (goRight)
+                        {
+                            Canvas.SetLeft(pacman, Canvas.GetLeft(pacman) - speed);
+                            goRight = false;
+                        }
+
+                        if (goLeft)
+                        {
+                            Canvas.SetLeft(pacman, Canvas.GetLeft(pacman) + speed);
+                            goLeft = false;
+                        }
+
+                        if (goUp)
+                        {
+                            Canvas.SetTop(pacman, Canvas.GetTop(pacman) + speed);
+                            goUp = false;
+                        }
+
+                        if (goDown)
+                        {
+                            Canvas.SetTop(pacman, Canvas.GetTop(pacman) - speed);
+                            goDown = false;
+                        }
+                    }
+                }
+            }
+        }
+        private void MoveGhost()
+        {
+            foreach (var x in MyCanvas.Children.OfType<Rectangle>())
+            {
+                Rect hitBox = new Rect(Canvas.GetLeft(x), Canvas.GetTop(x), x.Width, x.Height);
+
+                if ((string)x.Tag == "poisson")
+                {
+                    if (pacmanHitBox.IntersectsWith(hitBox) && x.Visibility == Visibility.Visible)
+                    {
+                        x.Visibility = Visibility.Hidden;
+                        score++;
+                    }
+                }
+
+                if ((string)x.Tag == "pieuvre")
+                {
+                    if (pacmanHitBox.IntersectsWith(hitBox))
+                    {
+                        gameTimer.Stop();
+                        gameover = true;
+                    }
+                    if (x.Name.ToString() == "orangeGuy")
+                    {
+                        Canvas.SetLeft(x, Canvas.GetLeft(x) - ghostSpeed);
+                    }
+                    if (x.Name.ToString() == "redGuy")
+                    {
+                        Canvas.SetLeft(x, Canvas.GetLeft(x) + ghostSpeed);
+                    }
+                    if (x.Name.ToString() == "pinkGuy")
+                    {
+                        Canvas.SetLeft(x, Canvas.GetLeft(x) + ghostSpeed);
+                    }
+                    currentGhostStep--;
+                    if (currentGhostStep < 1)
+                    {
+                        currentGhostStep = ghostMoveStep;
+                        ghostSpeed = -ghostSpeed;
+                    }
+                }
+
+            }
+        }
+        private void GameLoop(object sender, EventArgs e)
+        {
+            starImage.ImageSource = new BitmapImage(new Uri(AppDomain.CurrentDomain.BaseDirectory + "/images/treasurebox.jpg"));
+            txtScore.Content = "Score: " + score + "\n Press P to Pause and R to Resume";
+
+            movePacman();
+            MoveGhost();
+
+            if (score == 16)
+            {
+                for (int i = 0; i < 5; i++)
+                {
+                    Rectangle rec = new Rectangle()
+                    {
+                        Width = 30,
+                        Height = 30,
+                        Fill = starImage,
+                        //Stroke = Brushes.Red,
+                        //Visibility = Visibility.Hidden,
+                        StrokeThickness = 2,
+                        Tag = "star",
+                    };
+                    MyCanvas.Children.Add(rec);
+                    Canvas.SetTop(rec, 529);
+                    Canvas.SetLeft(rec, 220);
+
+                }
+            }
+            if (score == 47)
+            {
+                for (int i = 0; i < 5; i++)
+                {
+                    Rectangle rec = new Rectangle()
+                    {
+                        Width = 30,
+                        Height = 30,
+                        Fill = starImage,
+                        //Stroke = Brushes.Red,
+                        //Visibility = Visibility.Hidden,
+                        StrokeThickness = 2,
+                        Tag = "star",
+                    };
+                    MyCanvas.Children.Add(rec);
+                    Canvas.SetTop(rec, 199);
+                    Canvas.SetLeft(rec, 489);
+                }
+            }
+            if (score == 68)
+            {
+                for (int i = 0; i < 5; i++)
+                {
+                    Rectangle rec = new Rectangle()
+                    {
+                        Width = 30,
+                        Height = 30,
+                        Fill = starImage,
+                        //Stroke = Brushes.Red,
+                        Visibility = Visibility.Hidden,
+                        StrokeThickness = 2,
+                        Tag = "star",
+                    };
+                    MyCanvas.Children.Add(rec);
+                    Canvas.SetTop(rec, 50);
+                    Canvas.SetLeft(rec, 650);
+                }
+            }
+            if (score == 85)
+            {
+                GameOver("You Win, you collected all of the coins");
+            }
+            if (gameover)
+            {
+                txtScore.Content += "   Press R to Retry";
+            }
+        }
+        private void GameOver(string message)
+        {
+            gameTimer.Stop();
+            MessageBox.Show(message, "The Pac Man Game WPF MOO ICT");
+
+            //System.Diagnostics.Process.Start(Application.ResourceAssembly.Location);
+            //Application.Current.Shutdown();
         }
     }
 }
